da837df6954fa5b11c555a58b587f980e7ed85a2
Eric <ewade@stu.jsu.edu>
2022-10-03 14:24:32 -0500

    added reservation entity

---
+++ src/entities/reservation/reservation.entity.ts
@@ -0,0 +1,23 @@
+import dateColumnTransformer from 'src/helpers/dateColumnTransformer';
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Reservation extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  book_id: number;
+
+  @Column()
+  id: number;
+
+  @Column({ type: 'int', transformer: dateColumnTransformer })
+  start_time: number;
+
+  @Column({ type: 'int', transformer: dateColumnTransformer })
+  end_time: number;
+
+  @Column({ type: 'int', transformer: dateColumnTransformer })
+  entry_time: number;
+
+  @Column({ type: 'int', transformer: dateColumnTransformer })
+  exit_time: number;
+}
